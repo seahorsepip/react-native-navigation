2746863e61faa5408a1723e3ad9def6567917e95
Rotem M <rotemm@wix.com>
2018-06-10 15:39:34 +0300

    only run failed tests

--- e2e/ApplicationLifecycleTest.test.js
+++ e2e/ApplicationLifecycleTest.test.js
@@ -8,7 +8,7 @@ const { elementByLabel, elementById, sleep } = Utils;
 const IS_RELEASE = _.includes(process.argv, '--release');
 const KEY_CODE_R = 46;
 
-describe.skip('application lifecycle test', () => {
+describe.only('application lifecycle test', () => {
   beforeEach(async () => {
     await device.relaunchApp();
   });
